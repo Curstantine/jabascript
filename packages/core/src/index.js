1b4b841d52d11fef0c2376d732a1d467d34a3b59
Curstantine <curstantine@proton.me>
2024-07-15 01:47:41 +0530

    feat: add kotlin-style guarding functions to core

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,3 +1,67 @@
-/** */
-export function debounce() {
+/**
+ * Returns this value if it satisfies the given {@link predicate} or null, if it doesn't.
+ *
+ * @template T
+ * @param {T} source
+ * @param {(value: T) => boolean} predicate
+ *
+ * @returns T | null
+ * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/take-if.html
+ */
+export function takeIf(source, predicate) {
+	return predicate(source) ? source : null;
+}
+
+/**
+ * Returns this value if it doesn't satisfy the given {@link predicate} or null, if it does.
+ *
+ * @param {T} source
+ * @param {(value: T) => boolean} predicate
+ *
+ * @returns T | null
+ * @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/take-unless.html
+ */
+export function takeUnless(source, predicate) {
+	return takeIf(source, (x) => !predicate(x));
+}
+
+/**
+ * Returns the transformed value returned by {@link map} if {@link source} conforms to {@link predicate}
+ *
+ * @template T, Z
+ * @param {T} source
+ * @param {(value: T) => Z} map
+ * @param {(value: T) => boolean} predicate
+ *
+ * @returns {Z | null}
+ */
+export function takeMapped(source, map, predicate = (x) => x !== null && x !== undefined) {
+	if (!predicate(source)) return null;
+	return map(source);
+}
+
+/**
+ * Debounce a function caller in a {@link delay} window.
+ *
+ * @template T
+ * @param {(arg0: T) => void} func
+ * @param {number} delay - in ms
+ *
+ * @return {{ run: (arg0: T, customDelay?: number) => void, clear: () => void }}
+ */
+export function debounce(func, delay = 1000) {
+	/** @type { number } */
+	let timeoutId;
+
+	return {
+		run: (arg0, customDelay = delay) => {
+			clearTimeout(timeoutId);
+			timeoutId = setTimeout(() => func.call(this, arg0), customDelay);
+		},
+		clear: () => clearTimeout(timeoutId),
+	};
+}
+
+export async function wait(delay = 1000) {
+	await new Promise((resolve) => setTimeout(resolve, delay));
 }
